c89e7fd8858d7715c0db5779675e376865d1b5d6
Alex <84599917+GyverLibs@users.noreply.github.com>
2021-11-23 19:35:14 +0300

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,12 +1,16 @@
 #include <GyverHX711.h>
-GyverHX711 sensor(7, 8, HX_GAIN128_A);
+GyverHX711 sensor(3, 2, HX_GAIN64_A);
 // HX_GAIN128_A - канал А усиление 128
 // HX_GAIN32_B - канал B усиление 32
 // HX_GAIN64_A - канал А усиление 64
 
 void setup() {
   Serial.begin(9600);
-  sensor.tare();		// калибровка нуля
+  
+  // если тарирование при первом запуске -
+  // нужно выждать готовность датчика
+  delay(500);
+  sensor.tare();    // калибровка нуля
   
   //sensor.sleepMode(true);		// выключить датчик
   //sensor.sleepMode(false);	// включить датчик
